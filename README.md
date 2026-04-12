094db8ee6ea14226dd23a6caaf88dcf35793a0a1
vtf6259 <125233907+vtf6259@users.noreply.github.com>
2026-04-12 13:22:13 -0700

    Start implementing the protocol also bip39 wordlist

---
+++ README.md
@@ -0,0 +1,7 @@
+# Users
+This section is for users of ChatWS
+1. Your private key is your second password TREAT IT LIKE A PASSWORD! DO NOT SEND IT TO ANYONE!
+2. Your public key is for the server and users to send messages to you.
+- Your public key __needs__ to be shared for users to send messages.
+- You may be able to get it from your private key but it may be hard.
+- You can also recover it from someone you have chatted with or the server admin. KEEP YOUR PUBLIC KEY SAFE!
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,10 +3,17 @@ import { onMounted, ref, useTemplateRef } from 'vue';
 
   const serverURI = useTemplateRef("serverURI")
   const connectStatus = useTemplateRef("connectStatus")
+  const username = useTemplateRef("username")
+  const pubKeyRef = useTemplateRef("pgpPubKey")
+  const privKeyRef = useTemplateRef("pgpPrivKey")
+  let pgpPrivKey: string
+  let pgpPubKey: string
   let wsServer: WebSocket = new WebSocket("")
-  let messages = ref({
-    userMessages: []
-  })
+  let messages = ref(<string[]>[])
+  function genRecoverPhrase(pubkey: string, privkey: string) {
+    const encode = btoa(pubkey + ":" + privkey)
+    
+  }
   onMounted(() => {
     const messagesLocalStorage = localStorage.getItem("messages")
     if (messagesLocalStorage != null) {
@@ -17,20 +24,29 @@ import { onMounted, ref, useTemplateRef } from 'vue';
     websocket.addEventListener("open", () => {
       console.log("open")
       if (connectStatus.value == null) return
+      wsServer.send("USER " + username.value?.value)
       connectStatus.value.innerText = "Connected"
     })
     websocket.addEventListener("close", () => {
       if (connectStatus.value == null) return
       connectStatus.value.innerText = "Disconnected"
     })
+    websocket.addEventListener("message",(e) => {
+      messages.value.push(e.data)
+      let message = e.data.split(" ") 
+      console.log(message)
+    })
   }
   function connect() {
-    if (serverURI.value == undefined || connectStatus.value == null) {console.log("uhh"); return}
-    if (serverURI.value.value == "") {
+    if (serverURI.value == null || connectStatus.value == null || username.value == null || pubKeyRef.value == null || privKeyRef.value == null) {console.log("uhh"); return}
+    if (serverURI.value.value == "" || username.value.value == "" || privKeyRef.value.value == "" || pubKeyRef.value.value == "") {
       connectStatus.value.innerText = "No empty feilds!" 
       setTimeout(() => {if (connectStatus.value == null) {console.log("uhh"); return}; connectStatus.value.innerText = "Disconnected"}, 2000)
       return
     }
+    pgpPrivKey = atob(privKeyRef.value.value)
+    pgpPubKey = atob(pubKeyRef.value.value)
+    console.log(pgpPrivKey, "\n", pgpPubKey)
     wsServer = new WebSocket(serverURI.value?.value)
     connectStatus.value.innerText = "Connecting"
     addEventListeners(wsServer) // this is needed otherwise it does not work
@@ -39,8 +55,17 @@ import { onMounted, ref, useTemplateRef } from 'vue';
 
 <template>
   <div ref="connectStatus">Disconnected</div>
-  <input ref="serverURI" placeholder="Server URL"/>
-  <button v-on:click="connect()">Connect</button>
+  <div class="flexrow gap10px">
+    <input class="flexrowMember" ref="serverURI" placeholder="Server URL">
+    <input class="flexrowMember" ref="username" placeholder="Username">
+    <input class="flexrowMember" ref="pgpPubKey" placeholder="Base64 PGP Public Key">
+  <input class="flexrowMember" ref="pgpPrivKey" placeholder="Base64 PGP Private Key">
+ 
+    <button class="flexrowMember" v-on:click="connect()">Connect</button>
+  </div>
+  <ul>
+    <li v-for="message in messages">{{ message }}</li>
+  </ul>
 </template>
 
 <style scoped></style>
